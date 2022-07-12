f93a6c9e20fadd3cbd47f3ed039be0eb7d62999e
Settasit7 <108485042+Settasit7@users.noreply.github.com>
2022-07-12 11:21:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def function():
     st.title('Stocker')
     st.write('##')
     st.write('เว็บแอปทำนายราคาปิดหุ้นในอนาคตด้วยปัญญาประดิษฐ์')
+    st.write(':globe_with_meridians: [ภาษาไทย](https://translate.google.co.th/?hl=th&sl=th&tl=en&op=translate)')
 
     st.write('---')
 
